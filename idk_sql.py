58345bad824854eac9e57e5713a584b0afd6ebfb
denakuta <timurgazizov951@gmail.com>
2025-08-16 20:15:23 +0300

    new stage of educate

--- idk_sql.py
+++ idk_sql.py
@@ -1,12 +1,33 @@
 import sqlite3
 
 
-conn = sqlite3.connect(r'D:\trying_db\idk.sqlite')
+conn = sqlite3.connect('idk.db')
 cursor = conn.cursor()
 
+cursor.execute('DELETE FROM products WHERE id = ?', (2,))
+cursor.execute('INSERT INTO products (id, name, price) '
+               'VALUES (?, ?, ?)', (2, 'Скебоб', 2))
 cursor.execute('SELECT * FROM products')
-rows = cursor.fetchall()
+# cursor.execute('SELECT price,'
+#                ' AVG(price)'
+#                ' FROM products'
+#                ' GROUP BY price'
+#                ' HAVING AVG(price) > ?', (1.4,))
+# cursor.execute('SELECT * FROM products ORDER BY price DESC')
 
-print(rows)
+
+conn.commit()
+
+
+rows = cursor.fetchall()
+row_list = []
+for row in rows:
+    row_dict = {
+        'id': row[0],
+        'product': row[1],
+        'price': row[2]
+    }
+    row_list.append(row_dict)
+print(row_list[0])
 
 conn.close()
---
+++ test_fastapi.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, HTTPException
+from typing import Optional, List, Dict
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+
+class Post(BaseModel):
+    id: int
+    title: str
+    body: str
+    author: User
+
+users = [
+    {'id': 1, 'name': 'John', 'age': 34},
+    {'id': 2, 'name': 'Bob', 'age': 21},
+    {'id': 3, 'name': 'Alex', 'age': 25},
+]
+
+posts = [
+    {'id': 1, 'title': 'News 1', 'body': 'Text 1', 'author': users[2]},
+    {'id': 2, 'title': 'News 2', 'body': 'Text 2', 'author': users[1]},
+    {'id': 3, 'title': 'News 3', 'body': 'Text 3', 'author': users[0]},
+]
+
+@app.get('/items')
+async def items() -> List[Post]:
+    return [Post(**post) for post in posts]
+
+@app.get('/items/{id}')
+async def items(id: int) -> Post:
+    for post in posts:
+        if post['id'] == id:
+            return Post(**post)
+    raise HTTPException(status_code=404, detail='Post not found')
+
+
+@app.get('/search')
+async def search(post_id: Optional[int] = None) -> Dict[str, Optional[Post]]:
+    if post_id:
+        for post in posts:
+            if post['id'] == post_id:
+                return {'data': Post(**post)}
+        raise HTTPException(status_code=404, detail='Post not found')
+    else:
+        return {'data': None}
