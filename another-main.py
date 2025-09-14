adedd9365d6af646e415b961d0d8afb430168637
denakuta <timurgazizov951@gmail.com>
2025-09-14 06:10:15 +0300

    added db, a few methods

--- another-main.py
+++ another-main.py
@@ -1,15 +1,43 @@
+from pyexpat.errors import messages
+from typing import Optional
+
 from fastapi import FastAPI
-import uvicorn
+import sqlite3
+from pydantic import BaseModel
 
 app = FastAPI()
+with sqlite3.connect('notes.db') as conn:
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS notes ('
+                   'id INTEGER PRIMARY KEY,'
+                   'title TEXT,'
+                   'body TEXT)')
+    conn.commit()
+
+
+class Note(BaseModel):
+    title: str
+    body: str
+
 
 @app.get("/ping")
 def ping():
     return {"status": "OK"}
 
+
 @app.get("/")
 def root():
     return {"message": "Welcome to the API"}
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+@app.post('/notes')
+def post_notes(note: Note):
+    with sqlite3.connect('notes.db') as conn:
+        cursor.execute(
+            'INSERT INTO notes (title, body) VALUES (?, ?)',
+            (note.title, note.body)
+        )
+        conn.commit()
+        new_id = cursor.lastrowid
+    return {'Note created': 'Successfully'}
+
