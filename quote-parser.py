5d4be9e115f19533cd704a4dded5e9a339bd3f12
denakuta <timurgazizov951@gmail.com>
2026-02-17 02:45:25 +0300

    improved cycle

--- quote-parser.py
+++ quote-parser.py
@@ -1,34 +1,39 @@
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 import requests
 
-headers = {"User-Agent": UserAgent().random}
-response = requests.get('https://quotes.toscrape.com/', headers=headers)
 
-text = response.text
-soup = BeautifulSoup(text, 'html.parser')
-quotes = soup.find_all('div', class_='quote')
-
-
-def search():
+def search(quotes):
     for quote in quotes:
         phrase = quote.find('span', class_='text').text
         author = quote.find('small', class_='author').text
         tags = quote.find_all('a', class_='tag')
+
         print(f'{phrase[1:-1]}\n\nby: {author}\ntags: ', end='')
+
         for tag in tags:
             print(tag.text, end=' ')
         print('\n\n\n')
 
-search()
 
-try:
-    while True:
-        next_btn = soup.find("li", class_="next")
-        response = requests.get(f'https://quotes.toscrape.com/{next_btn.a['href']}', headers=headers)
-        text = response.text
-        soup = BeautifulSoup(text, 'html.parser')
-        quotes = soup.find_all('div', class_='quote')
-        search()
-except AttributeError:
-    print('\n\nQuotes ended!')
+url = 'https://quotes.toscrape.com/'
+headers = {"User-Agent": UserAgent().random}
+curr_url = url
+
+while curr_url:
+    response = requests.get(curr_url, headers=headers)
+
+    text = response.text
+    soup = BeautifulSoup(text, 'html.parser')
+    quotes = soup.find_all('div', class_='quote')
+
+    search(quotes)
+
+    next_btn = soup.find("li", class_="next")
+
+    if next_btn:
+        relative_path = next_btn.a['href']
+        curr_url = urljoin(curr_url, relative_path)
+    else:
+        curr_url = None
