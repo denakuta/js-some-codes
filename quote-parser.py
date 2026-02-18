ee337fdf5c119ee04edb155403af432ebde283bb
denakuta <timurgazizov951@gmail.com>
2026-02-18 13:49:25 +0300

    added cli tools like --pages(argparse)

--- quote-parser.py
+++ quote-parser.py
@@ -1,7 +1,16 @@
 from fake_useragent import UserAgent
-from bs4 import BeautifulSoup
 from urllib.parse import urljoin
+from bs4 import BeautifulSoup
 import requests
+import argparse
+
+parser = argparse.ArgumentParser(description='Scrape quotes from quotes.toscrape.com')
+parser.add_argument("--pages", type=int)
+parser.add_argument("--tag", type=str)
+
+args = parser.parse_args()
+
+lst = []
 
 
 def search(quotes):
@@ -10,18 +19,25 @@ def search(quotes):
         author = quote.find('small', class_='author').text
         tags = quote.find_all('a', class_='tag')
 
-        print(f'{phrase[1:-1]}\n\nby: {author}\ntags: ', end='')
-
         for tag in tags:
-            print(tag.text, end=' ')
-        print('\n\n\n')
+            lst.append(tag.text)
+        if args.tag != None:
+            if args.tag in lst:
+                print(f'{phrase[1:-1]}\n\nby: {author}\ntags:', ', '.join(lst), end='')
+                print('\n\n\n')
+        else:
+            print(f'{phrase[1:-1]}\n\nby: {author}\ntags: ', ', '.join(lst), end='')
+            print('\n\n\n')
+        lst.clear()
 
 
 url = 'https://quotes.toscrape.com/'
 headers = {"User-Agent": UserAgent().random}
 curr_url = url
 
+counter = 0
 while curr_url:
+    counter += 1
     response = requests.get(curr_url, headers=headers)
 
     text = response.text
@@ -37,3 +53,7 @@ while curr_url:
         curr_url = urljoin(curr_url, relative_path)
     else:
         curr_url = None
+
+    if args.pages:
+        if counter >= args.pages:
+            break
