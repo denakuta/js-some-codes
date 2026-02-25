24ea762634cecc1048fbebb47afe3eef9c443787
denakuta <timurgazizov951@gmail.com>
2026-02-25 14:14:21 +0300

    save

--- another_test.py
+++ another_test.py
@@ -9,14 +9,19 @@
 
 
 
-def show_task(task_num):
-    with open("task.txt", "r", encoding="utf-8") as file:
-        mylist = file.read().splitlines()
-        if 0 < task_num <= len(mylist):
-            print(mylist[task_num - 1])
-        else:
-            print("Такой задачи не существует.")
+# def show_task(task_num):
+#     with open("task.txt", "r", encoding="utf-8") as file:
+#         mylist = file.read().splitlines()
+#         if 0 < task_num <= len(mylist):
+#             print(mylist[task_num - 1])
+#         else:
+#             print("Такой задачи не существует.")
 
-def add_task(task_text):
-    with open("task.txt", "a", encoding="utf-8") as file:
-        file.write(task_text + "\n")
+# def add_task(task_text):
+#     with open("task.txt", "a", encoding="utf-8") as file:
+#         file.write(task_text + "\n")
+
+def sqrt(x):
+    return x ** 2
+
+print(sqrt(10))
--- test_fastapi.py
+++ test_fastapi.py
@@ -6,3 +6,14 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Welcome to the home page"}
+
+@app.get('/about')
+def about():
+    return {"message": "sdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"}
+
+@app.post('/login')
+def login(username: str, password: str):
+    if username == 'admin' and password == 'password':
+        return {"message": "Login successful"}
+    else:
+        return {"message": "Invalid credentials"}
